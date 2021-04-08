488ecfd8556d873ae668ffce8495dc83bd23a2a2
Patrick Kendall <patrickkendall@live.com>
2021-04-08 09:08:44 -0400

    made more components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
+import React from 'react';
 import './App.css';
 
-import HomePage from './homepage.component';
+import HomePage from './pages/homepage/homepage.component';
 
 function App() {
   return (
-    <div className='App'>
+    <div>
       <HomePage />
-  </div>
+    </div>
   );
 }
 
