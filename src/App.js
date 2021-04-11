2b272523fa4ee1f44453b4ead3dc548eb64855e0
Patrick Kendall <patrickkendall@live.com>
2021-04-11 15:07:42 -0400

    shop page

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
 import React from 'react';
-import './App.css';
-import { Route } from 'react-router-dom';
-import HomePage from './pages/homepage/homepage.component';
+import { Switch, Route } from 'react-router-dom';
 
-const HatsPage = () => (
-  <div>
-    <h1>Hats page</h1>
-  </div>
-)
+import './App.css';
+
+import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component.jsx';
 
 function App() {
   return (
     <div>
-     <Route exact path='/' component={HomePage} />
-     <Route exact path='/hats' component={HatsPage} />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/shop' component={ShopPage} />
+      </Switch>
     </div>
   );
 }
